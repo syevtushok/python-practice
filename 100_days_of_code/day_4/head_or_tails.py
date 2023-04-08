6c7436a5ce039aa02e2f524461887d160b08ece7
Serhii Yevtushok <ggazila@gmail.com>
2023-04-08 20:27:53 +0300

    head or tails

---
+++ 100_days_of_code/day_4/head_or_tails.py
@@ -0,0 +1,13 @@
+import random
+
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+
+# Write the rest of your code below this line 👇
+
+toss = random.randint(0, 1)
+
+if toss == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ 100_days_of_code/day_4/main.py
@@ -0,0 +1,6 @@
+import random
+
+randrange = random.randrange(1, 10)
+print(randrange)
+
+print(random.randint(0, 1))
